9b2954e31cb077ec96b5f802d7b1bd39c4bf2793
LoganAlston-tech <logan.alston@student.onslow.school.nz>
2026-09-24 12:24:58 +1200

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,43 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Color Swatch":return img`
+. 1 
+2 3 
+4 5 
+6 7 
+8 9 
+a b 
+c d 
+e f 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
